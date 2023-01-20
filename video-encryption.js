2ab1c472c08462eaf1dd43e02cbaea72be3d4e34
Ray000000 <st0105117@gmail.com>
2023-01-20 13:24:16 +0800

    t

--- video-encryption.js
+++ video-encryption.js
@@ -1,4 +1,4 @@
-var video_encryption = {
+/*var video_encryption = {
     mediaSource:new MediaSource(),
 
     checkSupported: function (cb) {
@@ -45,4 +45,18 @@ var video_encryption = {
         };
         xhr.send();
     }
+}; */
+
+var video = document.querySelector('video');
+var mediaSource = new MediaSource;
+video.src = URL.createObjectURL(mediaSource);
+mediaSource.addEventListener('sourceopen', sourceOpen)
+
+function sourceOpen () {
+  var mediaSource = this;
+  var sourceBuffer = mediaSource.addSourceBuffer('video/mp4; codecs="avc1.42E01E, avc1.640028, mp4a.40.2"');
+  sourceBuffer.addEventListener('updateend', function () {
+    mediaSource.endOfStream();
+  });
+  sourceBuffer.appendBuffer(buf);
 };
