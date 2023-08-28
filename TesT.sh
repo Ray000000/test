9bec803380298df4e5f1153993d4f797c905e057
Ray000000 <94271266+Ray000000@users.noreply.github.com>
2023-08-28 16:25:21 +0800

    Update TesT.sh

--- TesT.sh
+++ TesT.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# 此腳本將顯示一個簡單的選單
+
 echo "歡迎使用自定義 shell！"
 
 echo "請選擇您要執行的任務："
-echo "1. 更新並檢測系統"
-echo "2. 打開終端"
-echo "0. 退出"
+
+# 設定條目顏色
+echo "\e[38;5;34m1. 更新並檢測系統\e[0m"
+echo "\e[38;5;34m2. 打開終端\e[0m"
+echo "\e[38;5;34m0. 退出\e[0m"
 
 read choice
 
@@ -13,8 +17,9 @@ read choice
 
 case $choice in
   1)
+    # 更新並檢測系統
     apt update -y && apt full-upgrade -y && apt upgrade -y && apt autoremove -y && apt autoclean -y
-    \e[38;5;34mlsb_release -a\e[0m
+    lsb_release -a
     ;;
   2)
     gnome-terminal
